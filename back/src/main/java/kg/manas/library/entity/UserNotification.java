c770533bf548d64a6ac1da102b97f5531a642a34
Sanzhar <taalaibekuulusanzhar@gmail.com>
2022-10-22 00:54:11 +0600

    userNotification

---
+++ back/src/main/java/kg/manas/library/entity/UserNotification.java
@@ -0,0 +1,27 @@
+package kg.manas.library.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GeneratorType;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class UserNotification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "USER_NOTIFICATION_SEQ")
+    @SequenceGenerator(name = "USER_NOTIFICATION_SEQ", sequenceName = "USER_NOTIFICATION_SEQ", allocationSize = 1)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    boolean isViewed;
+}
---
+++ back/src/main/java/kg/manas/library/repository/UserNotification.java
@@ -0,0 +1,6 @@
+package kg.manas.library.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserNotification extends JpaRepository<kg.manas.library.entity.UserNotification,Long> {
+}
