268076754623f1602579060ce779eb38cc723a5d
Teamworker6 <akyl.nasirdinov@gmail.com>
2022-10-22 07:04:03 +0600

    bug fixes

--- back/src/main/java/kg/manas/library/LibraryApplication.java
+++ back/src/main/java/kg/manas/library/LibraryApplication.java
@@ -2,9 +2,7 @@ package kg.manas.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
 @SpringBootApplication
 public class LibraryApplication {
 
